5a134f3a6e8fac6048293cba7d0bb87a56e83785
Aswathi2307 <84922043+Aswathi2307@users.noreply.github.com>
2025-02-12 18:54:43 +0530

    Day 10

--- SBAAdvancedDependencies/src/main/java/com/example/demo/SbaAdvancedDependenciesApplication.java
+++ SBAAdvancedDependencies/src/main/java/com/example/demo/SbaAdvancedDependenciesApplication.java
@@ -1,17 +1,3 @@
-//package com.example.accessingdatajpa;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
-//@SpringBootApplication
-//public class SbaAdvancedDependenciesApplication {
-//
-//	public static void main(String[] args) {
-//		SpringApplication.run(SbaAdvancedDependenciesApplication.class, args);
-//	}
-//
-//}
-
 package com.example.demo;
 
 import org.slf4j.Logger;
